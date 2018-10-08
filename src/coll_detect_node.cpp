0c67808a544dfbfb1a3689672d5eff9aca5027da
Oscarsandstrom <oscar.sandstreom@gmail.com>
2018-10-08 15:10:05 +0200

    narrowed the lidar search to a cone in the front of the Permocar

--- src/coll_detect_node.cpp
+++ src/coll_detect_node.cpp
@@ -8,13 +8,15 @@
   ros::Subscriber scan_sub;
 
   bool stop;
-  float collision_threshold = 400.0;
+  float collision_threshold = 0.4;
+  int scan_start = 180;
+  int scan_stop = 361;
 
   void scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
   stop = false;
   //for loop to check every value in the ranges array from the LaserScan msg from the lidar node
-  for(int i = 0; i < scan->ranges.size(); i = i + 1)
+  for(int i = scan_start; i < scan_stop; i = i + 1)
   {
   
     //if function to test the values relative to our collision threshold. As soon a value is found that is lower than 0.4m the loop breaks and the publish function is started
@@ -40,7 +42,7 @@ int main(int argc, char** argv)
   
   ros::NodeHandle nh_;
   
-  stop_pub = nh_.advertise<std_msgs::Bool>("lidarstop", 1);
+  stop_pub = nh_.advertise<std_msgs::Bool>("lidar_stop", 1);
   scan_sub = nh_.subscribe<sensor_msgs::LaserScan>("scan", 1, scanCallback);
 
   ros::spin();
